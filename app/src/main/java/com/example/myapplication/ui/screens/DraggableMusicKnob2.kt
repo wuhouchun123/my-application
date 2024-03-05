ce2291921b2485bdf3fa0f15264d2c8d3a8173a7
dorunto <wuhouchun123@gmail.com>
2024-03-05 17:27:05 +0800

    3.4.0
    DraggableMusicKnob2（可调节音量的音乐组件(再写一遍)）-1.0.0

--- app/src/main/java/com/example/myapplication/ui/screens/DraggableMusicKnob2.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/DraggableMusicKnob2.kt
@@ -1,18 +1,28 @@
 package com.example.myapplication.ui.screens
 
 import android.view.MotionEvent
+import androidx.compose.foundation.Canvas
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import com.example.myapplication.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -21,25 +31,74 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
+import java.time.Clock.offset
 import kotlin.math.PI
 import kotlin.math.atan2
 
 @Composable
 fun DraggableMusicKnob2() {
     Box (
-        modifier = Modifier.size(300.dp),
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        MusicKnob2(modifier = Modifier.size(200.dp)) {
+        var volume by remember {
+            mutableStateOf(0f)
+        }
 
+        val barCount = 40
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .border(1.dp, Color.Green, RoundedCornerShape(10.dp))
+                .padding(30.dp)
+        ) {
+            MusicKnob2(modifier = Modifier.size(100.dp)) {
+                volume = it
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            VolumeBar2(
+                modifier = Modifier.fillMaxWidth().height(30.dp),
+                actionBars = (volume * barCount).toInt(),
+                barCount = barCount
+            )
         }
     }
+}
 
+@Composable
+fun VolumeBar2(
+    modifier: Modifier = Modifier,
+    actionBars: Int = 0,
+    barCount: Int = 10
+) {
+    BoxWithConstraints(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        val barWidth = remember {
+            constraints.maxWidth / ( 2f * barCount )
+        }
+
+        Canvas(modifier = modifier) {
+            for ( i in 0 until  barCount ) {
+                drawRect(
+                    topLeft = Offset(barWidth * i * 2f, 0f),
+                    color = if (i in 0 .. actionBars) Color.Red else Color.Gray,
+                    size = Size(barWidth, constraints.maxHeight.toFloat())
+                )
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -47,7 +106,7 @@ fun DraggableMusicKnob2() {
 fun MusicKnob2(
     modifier: Modifier = Modifier,
     limitAngle: Float = 25f,
-    onChanged: (Float) -> Unit
+    onChanged: (Float) -> Unit,
 ) {
     var centerX by remember {
         mutableStateOf(0f)
@@ -63,7 +122,7 @@ fun MusicKnob2(
     }
 
     var rotation by remember {
-        mutableStateOf(0f)
+        mutableStateOf(limitAngle)
     }
 
     Image(
@@ -79,7 +138,7 @@ fun MusicKnob2(
             .pointerInteropFilter { event ->
                 moveX = event.x
                 moveY = event.y
-                val angle = -atan2( centerX - moveX, centerY - moveY) * ( 180f / PI).toFloat()
+                val angle = -atan2(centerX - moveX, centerY - moveY) * (180f / PI).toFloat()
 
                 when (event.action) {
                     MotionEvent.ACTION_DOWN,
@@ -91,9 +150,13 @@ fun MusicKnob2(
                                 angle
                             }
                             rotation = fixedAngle
+                            val percentage =
+                                (fixedAngle - limitAngle) / (360f - limitAngle * 2f)
+                            onChanged(percentage)
                             true
                         } else false
                     }
+
                     else -> false
                 }
 
