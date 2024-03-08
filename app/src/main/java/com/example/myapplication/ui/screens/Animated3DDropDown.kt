82eeafc80991e4c16aa22bbd3dc36d25a99e9db1
dorunto <wuhouchun123@gmail.com>
2024-03-08 19:07:44 +0800

    3.6.0
    Animated3DDropDown（3D下拉动画）-1.0.0：完成点击动画效果

--- app/src/main/java/com/example/myapplication/ui/screens/Animated3DDropDown.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/Animated3DDropDown.kt
@@ -1,5 +1,9 @@
 package com.example.myapplication.ui.screens
 
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
@@ -9,6 +13,7 @@ import androidx.compose.ui.res.painterResource
 import com.example.myapplication.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -16,13 +21,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -41,8 +54,22 @@ fun Animated3DDropDown() {
 
 @Composable
 fun DropDown(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    initIsOpen: Boolean = false
 ) {
+    var isOpen by remember {
+        mutableStateOf(initIsOpen)
+    }
+
+    var rotatedX = animateFloatAsState(
+        targetValue = if (isOpen) 0f else -90f,
+        animationSpec = tween(
+            durationMillis = 150,
+            delayMillis = 0,
+            easing = FastOutLinearInEasing
+        )
+    )
+
     Column(
         modifier = modifier
             .fillMaxWidth(),
@@ -54,10 +81,15 @@ fun DropDown(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text("展开")
+            Text("坚持住dorunto")
             Icon(
                 imageVector = Icons.Default.ArrowDropDown,
-                contentDescription = "Open or close the drop down"
+                contentDescription = "Open or close the drop down",
+                modifier = Modifier
+                    .clickable {
+                        isOpen = !isOpen
+                    }
+                    .scale(if (isOpen) -1f else 1f)
             )
         }
         
@@ -65,6 +97,10 @@ fun DropDown(
 
         Box(
             modifier = Modifier
+                .graphicsLayer(
+                    transformOrigin = TransformOrigin(0.5f, 0f),
+                    rotationX = rotatedX.value
+                )
                 .background(Color.Blue)
                 .fillMaxWidth()
                 .height(200.dp),
